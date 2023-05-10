a031f2880574853cb45e9d1ac61ede8f74b25beb
leonraz58 <leonraz58@gmail.com>
2023-05-10 22:10:25 +0300

    8-9

---
+++ javascript/8kyu/9.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+function repeatStr (n, s) {
+    //return '';
+    let s2 = '';
+    for (let i = 1; i<=n; i++){
+      s2 += s;
+    }
+    return s2;
+  }
