9707ea0e5d577374efd6b744fff2e11626e9bf8a
leonraz58 <leonraz58@gmail.com>
2023-05-11 20:37:07 +0300

    11

---
+++ javascript/8kyu/11.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/515e271a311df0350d00000f
+function squareSum(numbers){
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+      sum += numbers[i]*numbers[i];
+    }
+    return sum;
+  }
