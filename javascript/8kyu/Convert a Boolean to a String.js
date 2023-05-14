00adff65a54231485cde810f52f4424ede0eaf3f
leonraz58 <leonraz58@gmail.com>
2023-05-14 23:22:21 +0300

    8 kyu js Convert a Boolean to a String

---
+++ javascript/8kyu/Convert a Boolean to a String.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd
+function booleanToString(b){
+    //your code here
+    if (b === true) {
+      return "true"; 
+    } else {
+      return "false";
+    }
+  }
