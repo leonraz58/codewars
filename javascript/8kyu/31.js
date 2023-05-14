64d7c106ede44591ec226538645c0432a702dea8
leonraz58 <leonraz58@gmail.com>
2023-05-14 23:20:24 +0300

    8-31

---
+++ javascript/8kyu/31.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+function arrayPlusArray(arr1, arr2) {
+    sum=0;
+    for (i=0; i < arr1.length; i++){
+      sum+=arr1[i];
+    }
+    for (i=0; i < arr2.length; i++){
+      sum+=arr2[i];
+    }
+    return sum; //something went wrong
+  }
