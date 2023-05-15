229a29c047a250e39b1f833b244f0fc14992d4f0
leonraz58 <leonraz58@gmail.com>
2023-05-15 22:28:18 +0300

    8 kyu js Are You Playing Banjo?

---
+++ javascript/8kyu/Are You Playing Banjo.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if (name[0] === 'R' || name[0] === 'r'){
+      return name +" plays banjo";  
+    } else return name +" does not play banjo";  
+    
+  }
